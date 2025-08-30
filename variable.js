b0297ba03f3f1ea07aa962657cf4b135c56bf0e0
Rakhi mishra <rakhimishra9572@gmail.com>
2025-08-30 22:03:52 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,11 @@
+var a=32;
+var b=50;
+var c=34;
+var d=45;
+var e=56;
+
+console.log("sum  "+a + "+"  + b + "%" +c+"-" +  +d+ "*" +e+"/" +" = "+ (a+b-c*d/e));
+// console.log(`sum of $(a) and $(b) = $(a+b)`)
+//console.log(a-b)
+//console.log(a*b)
+//console.log(a/b)
